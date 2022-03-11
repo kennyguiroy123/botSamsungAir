da674182826480f7830d0c63f3b99157f0388621
kennyguiroy123 <kenny.guiroy@epsi.fr>
2022-03-11 14:29:08 +0100

    Test
    Pour finir les test nécessité de finir la classe MotionStub

---
+++ src/main/java/bot/air/model/MotionStub.java
@@ -0,0 +1,5 @@
+package bot.air.model;
+
+public class MotionStub {
+
+}
--- src/test/java/botSamsungAir/TestMotionStubs.java
+++ src/test/java/botSamsungAir/TestMotionStubs.java
@@ -1,5 +1,39 @@
 package botSamsungAir;
 
-public class TestMotionStubs {
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import bot.air.model.MotionStub;
+
+public class TestMotionStubs {
+	
+	private MotionStub motionStub;
+	
+	@Test
+	public void InitializationWithInvalidePattern_ThrowsArgumentOutOfRangeException() {
+		//assertThrows(ArgumentOutOfRangeException, motionStub) manque MotionStub avec les exceptions pour finir
+	}
+	
+	@Test
+	public void InitializationWithNoInitialPosition_ThrowsArgumentException() {
+		//assertThrows(ArgumentException, motionStub) manque MotionStub avec les exceptions pour finir
+	}
+	
+	@Test
+	public void InitializationWithTooManyPosition_ThrowsArgumentException() {
+		//assertThrows(ArgumentException, motionStub) manque MotionStub avec les exceptions pour finir
+	}
+	
+	@Test
+	public void InitializationWithNonRectangle_FillWithEmpty() {
+		this.motionStub = new MotionStub();
+		//assertTrue(motionStub.move(1));
+	}
+	
+	
+	
+	
 }
